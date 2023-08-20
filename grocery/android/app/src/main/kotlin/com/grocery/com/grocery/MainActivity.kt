1d0fe5796fe1de1ecf8aa0da6072d105273d5d27
AkashShrestha <akash@vurilo.com>
2023-08-20 20:10:36 +0545

    initalize project

--- grocery/android/app/src/main/kotlin/com/grocery/com/grocery/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.grocery.com.grocery
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ groceryfrontend/android/app/src/main/kotlin/com/grocery/groceryfrontend/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.grocery.groceryfrontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
